2c6a3c7c31f433e4dca620a97ca1c496400a02d5
Emile-Nsengimana <emilereas7@gmail.com>
2020-06-26 16:25:02 +0200

    Updates

---
+++ precache-manifest.8ca5705eb41d36c8af91ea4ab9f62dca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d89b2e5a18dc34de614bd5fad98e39",
+    "url": "/recipe-box/index.html"
+  },
+  {
+    "revision": "0ef4bc43bf9f48eeef50",
+    "url": "/recipe-box/static/css/main.c8910ef5.chunk.css"
+  },
+  {
+    "revision": "397ad1c355e76a4dfc97",
+    "url": "/recipe-box/static/js/2.cd769018.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-box/static/js/2.cd769018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef4bc43bf9f48eeef50",
+    "url": "/recipe-box/static/js/main.96a3d5f4.chunk.js"
+  },
+  {
+    "revision": "34b673932c5f5785b601",
+    "url": "/recipe-box/static/js/runtime-main.7648288e.js"
+  },
+  {
+    "revision": "4585338408890ae1b7c109ed3801a55f",
+    "url": "/recipe-box/static/media/logo.45853384.png"
+  }
+]);
