541c60d7c40036a96fb8fbbc57b0991e2306645b
Emile-Nsengimana <emilereas7@gmail.com>
2020-06-27 20:40:55 +0200

    Updates

--- precache-manifest.0820cdc33982ec34208ea87b279b9350.js
+++ precache-manifest.0820cdc33982ec34208ea87b279b9350.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db73fcebeae3bf986057b6d54c576c2e",
+    "revision": "c478063b90827d11e3ac597e9ffdaf0c",
     "url": "/recipe-box/index.html"
   },
   {
-    "revision": "4f3144a86d3bb15b389d",
+    "revision": "c05f4578ece5515ce020",
     "url": "/recipe-box/static/css/main.c8910ef5.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/recipe-box/static/js/2.9390a45f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f3144a86d3bb15b389d",
-    "url": "/recipe-box/static/js/main.4bd52feb.chunk.js"
+    "revision": "c05f4578ece5515ce020",
+    "url": "/recipe-box/static/js/main.2caa5b59.chunk.js"
   },
   {
     "revision": "34b673932c5f5785b601",
